eeb79376bec479b6cfa44a8a285547e58900aa13
Sergio Solis <sergiosolis@MacBook-Pro-de-Sergio.local>
2024-08-19 19:54:50 -0300

    speedinsights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return(
+  return (
     <Layout>
       <Component {...pageProps} />
+      <SpeedInsights />
     </Layout>
 
   )
