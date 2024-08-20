4c6e00649ac75ef0d213a0b1f50c2e24165a5a8a
Sergio Solis <sergiosolis@MacBook-Pro-de-Sergio.local>
2024-08-20 19:11:02 -0300

    change head name

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,6 +8,7 @@ export default function Layout({ children }) {
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Portfolio Sergio Solis</title>
             </Head>
 
             <main className={`w-full ${ router.pathname ==="/" && "pt-8"} min-h-screen gap-1 dark:bg-gray-800 bg-gray-200 dark:text-gray-300 flex flex-col items-center`}>
